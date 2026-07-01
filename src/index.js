ba2e4516b5f0a7be18fb1def2b29001315d4b175
FUN_GAMING <sanjuyadav47150@gmail.com>
2026-07-01 18:10:20 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+
+console.log("======================================");
+console.log("        Astryx 2.0 Starting...");
+console.log("======================================");
