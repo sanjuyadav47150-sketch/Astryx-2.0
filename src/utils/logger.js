bae154c709845d6700c67c90ce4c8ba804d7ecfb
FUN_GAMING <sanjuyadav47150@gmail.com>
2026-07-01 18:14:26 +0530

    Create logger.js

---
+++ src/utils/logger.js
@@ -0,0 +1,34 @@
+const { createLogger, format, transports } = require("winston");
+
+const logger = createLogger({
+    level: "info",
+
+    format: format.combine(
+        format.timestamp({
+            format: "YYYY-MM-DD HH:mm:ss"
+        }),
+
+        format.errors({
+            stack: true
+        }),
+
+        format.printf(info => {
+            return `[${info.timestamp}] [${info.level.toUpperCase()}] ${info.message}`;
+        })
+    ),
+
+    transports: [
+        new transports.Console(),
+
+        new transports.File({
+            filename: "logs/error.log",
+            level: "error"
+        }),
+
+        new transports.File({
+            filename: "logs/combined.log"
+        })
+    ]
+});
+
+module.exports = logger;
